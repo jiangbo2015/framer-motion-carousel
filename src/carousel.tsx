62d884c2377fa6ceb9d2c781f2a7d7bf8eaab93f
jiangbo2015 <772051431@qq.com>
2023-08-29 11:23:24 +0800

    fix: #11

--- src/carousel.tsx
+++ src/carousel.tsx
@@ -35,102 +35,127 @@ const Contaier = React.forwardRef<
     </div>
 ))
 
-export const Carousel = React.forwardRef(({
-    children,
-    renderArrowLeft,
-    renderArrowRight,
-    renderDots,
-    autoPlay = true,
-    interval = 2000,
-    loop = true,
-}: CarouselProps, ref: React.Ref<CarouselRef>) => {
-    const x = useMotionValue(0)
-    const containerRef = React.useRef<HTMLDivElement>(null)
-    const [index, setIndex] = React.useState(0)
+export const Carousel = React.forwardRef(
+    (
+        {
+            children,
+            renderArrowLeft,
+            renderArrowRight,
+            renderDots,
+            autoPlay = true,
+            interval = 2000,
+            loop = true,
+        }: CarouselProps,
+        ref: React.Ref<CarouselRef>
+    ) => {
+        const x = useMotionValue(0)
+        const containerRef = React.useRef<HTMLDivElement>(null)
+        const [index, setIndex] = React.useState(0)
 
-    const calculateNewX = () =>
-        -index * (containerRef.current?.clientWidth || 0)
+        const calculateNewX = () =>
+            -index * (containerRef.current?.clientWidth || 0)
 
-    const handleEndDrag = (e: Event, dragProps: PanInfo) => {
-        const clientWidth = containerRef.current?.clientWidth || 0
+        const handleEndDrag = (e: Event, dragProps: PanInfo) => {
+            const clientWidth = containerRef.current?.clientWidth || 0
 
-        const { offset } = dragProps
+            const { offset } = dragProps
 
-        if (offset.x > clientWidth / 4) {
-            handlePrev()
-        } else if (offset.x < -clientWidth / 4) {
-            handleNext()
-        } else {
-            animate(x, calculateNewX(), transition)
+            // fix:https://github.com/jiangbo2015/framer-motion-carousel/issues/11
+            // stop start slide and end slide move
+            if (
+                (index + 1 === childrens.length && offset.x < 0) ||
+                (index === 0 && offset.x > 0)
+            ) {
+                animate(x, calculateNewX(), transition)
+                return
+            }
+
+            if (offset.x > clientWidth / 4) {
+                handlePrev()
+            } else if (offset.x < -clientWidth / 4) {
+                handleNext()
+            } else {
+                animate(x, calculateNewX(), transition)
+            }
         }
-    }
 
-    const childrens = React.Children.toArray(children)
+        const childrens = React.Children.toArray(children)
 
-    const handleNext = () => {
-        const idx = loop ? 0 : index;
-        setIndex(index + 1 === childrens.length ? idx : index + 1)
-    }
-
-    const handlePrev = () => {
-        const idx = loop ? childrens.length - 1: 0;
-        setIndex(index - 1 < 0 ? idx : index - 1)
-    }
-
-    React.useEffect(() => {
-        const controls = animate(x, calculateNewX(), transition)
-        return controls.stop
-    }, [index])
-
-    React.useEffect(() => {
-        if (!autoPlay) {
-            return
+        const handleNext = () => {
+            const idx = loop ? 0 : index
+            setIndex(index + 1 === childrens.length ? idx : index + 1)
         }
-        const timer = setInterval(() => handleNext(), interval)
-        return () => clearInterval(timer)
-    }, [handleNext, interval])
 
-    React.useImperativeHandle(ref, () => {
-        return {
-            handleNext,
-            handlePrev,
-            setIndex,
-        };
-      }, [index]);
+        const handlePrev = () => {
+            const idx = loop ? childrens.length - 1 : 0
+            setIndex(index - 1 < 0 ? idx : index - 1)
+        }
 
-    return (
-        <Contaier ref={containerRef}>
-            {childrens.map((child, i) => (
-                <Slider onDragEnd={handleEndDrag} totalSliders={childrens.length} x={x} i={i} key={i}>
-                    {child}
-                </Slider>
-            ))}
-            {/* left arrow */}
-            {renderArrowLeft ? (
-                renderArrowLeft({ handlePrev, activeIndex: index })
-            ) : (
-                <Arrow left onClick={handlePrev}>
-                    &larr;
-                </Arrow>
-            )}
+        React.useEffect(() => {
+            const controls = animate(x, calculateNewX(), transition)
+            return controls.stop
+        }, [index])
 
-            {/* right arrow */}
-            {renderArrowRight ? (
-                renderArrowRight({ handleNext, activeIndex: index })
-            ) : (
-                <Arrow onClick={handleNext}>&rarr;</Arrow>
-            )}
+        React.useEffect(() => {
+            if (!autoPlay) {
+                return
+            }
+            const timer = setInterval(() => handleNext(), interval)
+            return () => clearInterval(timer)
+        }, [handleNext, interval])
 
-            {/* dots */}
-            {renderDots ? (
-                renderDots({ setActiveIndex: setIndex, activeIndex: index })
-            ) : (
-                <Dots
-                    length={childrens.length}
-                    setActiveIndex={setIndex}
-                    activeIndex={index}
-                />
-            )}
-        </Contaier>
-    )
-})
+        React.useImperativeHandle(
+            ref,
+            () => {
+                return {
+                    handleNext,
+                    handlePrev,
+                    setIndex,
+                }
+            },
+            [index]
+        )
+
+        return (
+            <Contaier ref={containerRef}>
+                {childrens.map((child, i) => (
+                    <Slider
+                        onDragEnd={handleEndDrag}
+                        totalSliders={childrens.length}
+                        x={x}
+                        i={i}
+                        key={i}
+                    >
+                        {child}
+                    </Slider>
+                ))}
+                {/* left arrow */}
+                {renderArrowLeft ? (
+                    renderArrowLeft({ handlePrev, activeIndex: index })
+                ) : (
+                    <Arrow left onClick={handlePrev}>
+                        &larr;
+                    </Arrow>
+                )}
+
+                {/* right arrow */}
+                {renderArrowRight ? (
+                    renderArrowRight({ handleNext, activeIndex: index })
+                ) : (
+                    <Arrow onClick={handleNext}>&rarr;</Arrow>
+                )}
+
+                {/* dots */}
+                {renderDots ? (
+                    renderDots({ setActiveIndex: setIndex, activeIndex: index })
+                ) : (
+                    <Dots
+                        length={childrens.length}
+                        setActiveIndex={setIndex}
+                        activeIndex={index}
+                    />
+                )}
+            </Contaier>
+        )
+    }
+)
